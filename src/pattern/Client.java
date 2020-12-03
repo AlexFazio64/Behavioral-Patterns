d5e5e0f0cbdb39d7e1b50d58c305f798be58c365
Alessandro Fazio <alexfazio64@gmail.com>
2020-12-03 10:01:11 +0100

    Initial Setup

---
+++ src/pattern/Client.java
@@ -0,0 +1,23 @@
+package pattern;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("view/root.fxml"));
+		primaryStage.setScene(new Scene(loader.load()));
+		primaryStage.setTitle("TITLE");
+		primaryStage.setResizable(false);
+		primaryStage.setWidth(600);
+		primaryStage.setHeight(400);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/pattern/controller/RootController.java
@@ -0,0 +1,4 @@
+package pattern.controller;
+
+public class RootController {
+}
